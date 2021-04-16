3a763c0ea5d532c4c1910e013b61604175c33429
Friiia <58567234+Friiia@users.noreply.github.com>
2021-04-16 18:37:11 +0300

    Update nopeusLaskuriTesti.js

--- nopeusLaskuriTesti.js
+++ nopeusLaskuriTesti.js
@@ -1,29 +1,27 @@
 function laske(){
     let print = "";
-    document.getElementById("tulostusAlue").innerHTML=""
-    var nopeus = document.getElementById("nopeus").value;
-    var tieNopeus = document.getElementById("tieNopeus").value;
-    var nopeusOli = nopeus < 60;
+    document.getElementById("tulostusAlue").innerHTML="";
+    let nopeus = document.getElementById("nopeus").value;
+    let tieNopeus = document.getElementById("tieNopeus").value;
     let pieniYli = tieNopeus + 10;
     let vahanYli = tieNopeus + 15;
     let paljonYli = tieNopeus + 20;
     
-    if (nopeus == pieniYli){
-        print = "Määrää liikennesakko 100e";
-       }
-
-       else if (nopeus == vahanYli){
-        print = "Määrää liikennesakko 170e";
-                }
-    else if (nopeus == paljonYli){
-    print = "Määrää liikennesakko 200e";
+    if (nopeus < tieNopeus){
+    print = " Hyvää matkaa";
     }
+    else if (nopeus > pieniYli){
+        print = "Määrää liikennesakko 100e";
+       
+    }
+   else if (nopeus > vahanYli){
+        print = "Määrää liikennesakko 170e";
+   }     
+ else  if (nopeus > paljonYli){
+    print = "Määrää liikennesakko 200e";
+ }
 else if (nopeus > tieNopeus) {
     print = " Tarkista tulot ja määrää päiväsakko";
-    }
-    else {
-        print = " Hyvää matkaa";
-        }
-
+}
 document.getElementById("tulostusAlue").innerHTML = print;
 }
